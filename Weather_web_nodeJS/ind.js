cbe28826e375932e419b0a82df84378d88826178
Kuldip Rupsangbhai Parmar <116505382+PKR9759@users.noreply.github.com>
2023-08-08 18:19:28 +0530

    Updateed some comments

--- Weather_web_nodeJS/ind.js
+++ Weather_web_nodeJS/ind.js
@@ -4,8 +4,8 @@ const requests= require('requests');
 
 const home=fs.readFileSync("index.html",'utf-8');
 
-const replVal=(tmp,real)=>{
-    let tempr=tmp.replace("[%temp%]",(real.main.temp-273.15).toFixed(2));
+const replVal=(tmp,real)=>{//Here we used tmpr variable to store resultant dtaa after replacement becuse replace not change original data but return it
+    let tempr=tmp.replace("[%temp%]",(real.main.temp-273.15).toFixed(2));// tmp is our html file and real is our api data
      tempr=tempr.replace("[%city%]",real.name);
      tempr=tempr.replace("[%country%]",real.sys.country);
      return tempr;
