099cfe2dffa18ae2e06049d6ac4aa8962e26a1ee
Kuldip Rupsangbhai Parmar <116505382+PKR9759@users.noreply.github.com>
2023-08-08 17:56:34 +0530

    Create ind.js

---
+++ Weather_web_nodeJS/ind.js
@@ -0,0 +1,36 @@
+const http= require('http');
+const fs= require('fs');
+const requests= require('requests');
+
+const home=fs.readFileSync("index.html",'utf-8');
+
+const replVal=(tmp,real)=>{
+    let tempr=tmp.replace("[%temp%]",(real.main.temp-273.15).toFixed(2));
+     tempr=tempr.replace("[%city%]",real.name);
+     tempr=tempr.replace("[%country%]",real.sys.country);
+     return tempr;
+}
+const server=http.createServer((req, res) => {
+
+    if(req.url=="/"){
+        requests("https://api.openweathermap.org/data/2.5/weather?q=Nadiad&appid=bd1b1dea813f59ea88547a79ed2304e1",)
+        .on("data", (data) =>{ 
+            const objd=JSON.parse(data);
+            let arr=[objd]
+            // console.log(arr);
+
+            const realTimeData=arr.map(val=> replVal(home, val)).join("");
+            res.write(realTimeData);
+        }) 
+        .on("end", (err) =>{
+            if(err) return console.log("Connection closed due to erorrs",err);
+            // console.log('end');
+            res.end();
+        });   
+    } 
+});
+server.listen(8000,"127.0.0.1");
+
+
+
+
